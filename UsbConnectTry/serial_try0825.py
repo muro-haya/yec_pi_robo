86f90b4e5bb8e8645ce47815f77f265d67ce6546
eaglraune <miyano1996@yahoo.co.jp>
2024-08-25 16:18:04 +0900

    create serialmodule in serial_try0825
    log
    -----------------------------
    Received: @000:000735
    Sent: @000:000740
    
    Received: @000:000740
    Received: @501:000740
    Received: @500:991000
    Sent: @000:000745
    
    Received: @000:000745
    Sent: @000:000750
    
    Received: @000:000750
    Sent: @000:000755
    
    Received: @000:000755
    Sent: @000:000760
    
    Received: @000:000760
    Sent: @000:000765
    
    Received: @000:000765
    Sent: @000:000770
    
    Received: @000:000770
    Sent: @000:000775
    
    Received: @000:000775
    Sent: @000:000780
    
    Received: @000:000780
    Sent: @000:000785
    
    Received: @000:000785
    Sent: @000:000790
    
    Received: @000:000790
    Received: @501:000790
    Received: @500:992000
    Sent: @000:000795
    
    Received: @000:000795
    Sent: @000:000800
    
    Received: @000:000800
    Sent: @000:000805
    
    Received: @000:000805
    Sent: @000:000810
    
    Received: @000:000810
    Sent: @000:000815
    
    Received: @000:000815
    Sent: @000:000820
    
    Received: @000:000820
    Sent: @000:000825
    
    Received: @000:000825
    Sent: @000:000830
    
    Received: @000:000830
    Sent: @000:000835
    
    Received: @000:000835
    Sent: @000:000840
    
    Received: @000:000840
    Received: @501:000840
    Received: @500:993000
    Sent: @000:000845
    
    Received: @000:000845
    Sent: @000:000850

---
+++ UsbConnectTry/serial_try0825.py
@@ -0,0 +1,48 @@
+import serial
+import time
+import threading
+
+# シリアルポートの設定
+SERIAL_PORT = '/dev/ttyACM0'
+BAUD_RATE = 9600
+
+# シリアルポートをオープン
+ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
+
+def send_data(message):
+    """指定されたメッセージをシリアルポートに送信する"""
+    ser.write(message.encode('utf-8'))
+    print(f"Sent: {message}")
+
+def receive_data():
+    """シリアルポートからデータを受信する"""
+    if ser.in_waiting > 0:  # データが待機中かどうかを確認
+        data = ser.read(ser.in_waiting).decode('utf-8').strip()
+        if data:
+            print(f"Received: {data}")
+
+def count_up(count, command):
+    """100msごとにカウントアップした数字をシリアルポートに送信する"""
+    message = f"@{command:03d}:{count:06d}\n"
+    send_data(message)
+    count += 5
+    # 次のタイマーをセット
+    threading.Timer(0.1, count_up, args=(count, command)).start()
+
+try:
+    # 初期値
+    initial_count = 0
+    command = 0
+    
+    # カウントアップ処理を開始
+    count_up(initial_count, command)
+    
+    # データ受信処理を実行
+    while True:
+        receive_data()
+        time.sleep(0.01)  # CPU負荷を軽減するために少し待機
+
+except KeyboardInterrupt:
+    print("ユーザーによって中断されました")
+finally:
+    ser.close()  # シリアルポートを閉じる
