a6c4b5a697d4c63e7ec1b627b5f7fe37c2e0fd9a
muro-haya <haya0908to@yahoo.co.jp>
2024-09-07 10:30:03 +0900

    fix communication func and add rrecognige red func

--- miyano_pi_pj/camera.py
+++ miyano_pi_pj/camera.py
@@ -13,51 +13,68 @@ def cyc_camera():
         # キャプチャの実施
         ret, frame = cap.read()
         if ret == True:
-            gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-            # edges = cv2.Canny(gray, 50, 150)
-
             hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
             lower_blue = np.array([50, 150, 100])
             upper_blue = np.array([140, 255, 255])
+
+            lower_red1 = np.array([0, 100, 100])
+            upper_red1 = np.array([40, 255, 255])
+            lower_red2 = np.array([160, 100, 100])
+            upper_red2 = np.array([180, 255, 255])
 
             # Get the image dimensions
             height, width = frame.shape[:2]
             # Create a white image of the same size as the original image
             white_image = np.ones((height, width, 3), dtype=np.uint8) * 255
 
-            mask = cv2.inRange(hsv, lower_blue, upper_blue)
+            mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
 
-            white_result = cv2.bitwise_and(white_image, white_image, mask=mask)
-            # result = cv2.bitwise_and(frame, frame, mask=mask)
+            mask_red1 = cv2.inRange(hsv, lower_red1, upper_red1)
+            mask_red2 = cv2.inRange(hsv, lower_red2, upper_red2)
+            mask_red = mask_red1 | mask_red2
+
+            white_result_blue = cv2.bitwise_and(white_image, white_image, mask=mask_blue)
+            white_result_red  = cv2.bitwise_and(white_image, white_image, mask=mask_red)
 
             # Define a kernel for morphological operations
             kernel = np.ones((5, 5), np.uint8)
-            # Apply erosion
-            eroded_image = cv2.erode(white_result, kernel, iterations=1)
-            # Apply dilation
-            dilated_image = cv2.dilate(eroded_image, kernel, iterations=1)
+            eroded_image_blue  = cv2.erode(white_result_blue, kernel, iterations=1)     # Apply erosion
+            dilated_image_blue = cv2.dilate(eroded_image_blue, kernel, iterations=1)    # Apply dilation
+            eroded_image_red   = cv2.erode(white_result_red, kernel, iterations=1)      # Apply erosion
+            dilated_image_red  = cv2.dilate(eroded_image_red, kernel, iterations=1)     # Apply dilation
+
             kernel = np.ones((20, 20), np.uint8)
-            # Apply erosion
-            eroded_image2 = cv2.erode(dilated_image, kernel, iterations=1)
-            # Apply dilation
-            dilated_image2 = cv2.dilate(eroded_image2, kernel, iterations=1)
+            eroded_image2_blue  = cv2.erode(dilated_image_blue, kernel, iterations=1)   # Apply erosion
+            dilated_image2_blue = cv2.dilate(eroded_image2_blue, kernel, iterations=1)  # Apply dilation
+            eroded_image2_red   = cv2.erode(dilated_image_red, kernel, iterations=1)    # Apply erosion
+            dilated_image2_red  = cv2.dilate(eroded_image2_red, kernel, iterations=1)   # Apply dilation
 
-            gray2 = cv2.cvtColor(dilated_image2,cv2.COLOR_BGR2GRAY)
-            edges2 = cv2.Canny(gray2, 50, 150)
-            contours2, _ = cv2.findContours(edges2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            cv2.drawContours(dilated_image2, contours2, -1, (0,255,0), 2)
-
-            for contour in contours2:
+            gray = cv2.cvtColor(dilated_image2_blue,cv2.COLOR_BGR2GRAY)
+            edges = cv2.Canny(gray, 50, 150)
+            contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            # cv2.drawContours(dilated_image2_blue, contours, -1, (0,255,0), 2)
+            for contour in contours:
                 epsilon = 0.05 * cv2.arcLength(contour, True)
                 approx = cv2.approxPolyDP(contour, epsilon, True)
                 if len(approx) == 4:
-                    cv2.drawContours(frame, [approx], -1, (0,255,0), 2)
+                    cv2.drawContours(frame, [approx], -1, (255,0,0), 2)
+                    
+            gray = cv2.cvtColor(dilated_image2_red,cv2.COLOR_BGR2GRAY)
+            edges = cv2.Canny(gray, 50, 150)
+            contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            cv2.drawContours(dilated_image2_red, contours, -1, (0,255,0), 2)
+            for contour in contours:
+                epsilon = 0.05 * cv2.arcLength(contour, True)
+                approx = cv2.approxPolyDP(contour, epsilon, True)
+                if len(approx) == 4:
+                    cv2.drawContours(frame, [approx], -1, (0,0,255), 2)
 
             cv2.imshow("camera", frame)
-            # cv2.imshow("edge", dilated_image2)
-            # cv2.imshow("mask", result)
-            # cv2.imshow("white_mask", white_result)
+            # cv2.imshow("edge", dilated_image2_blue)
+            # cv2.imshow("edge", dilated_image2_red)
+            # cv2.imshow("white_mask", white_result_blue)
+            # cv2.imshow("white_mask", white_result_red)
 
     if cv2.waitKey(1) & 0xff == ord('q'):
         return True
--- miyano_pi_pj/serial2SPIKE.py
+++ miyano_pi_pj/serial2SPIKE.py
@@ -18,56 +18,57 @@ buffer_num = 0
 
 # 通信データ構造
 class CommData:
-    def __init__(self, comm_cnt, comm_cyc, comm_cmd, comm_data):
+    def __init__(self, comm_cnt, comm_cyc, comm_cmd, comm_sign, comm_data):
         self.comm_cnt = comm_cnt
         self.comm_cyc = comm_cyc
         self.comm_cmd = comm_cmd
+        self.comm_sign = comm_sign # 0:unsign 1:sign
         self.comm_data = comm_data
         
 # 送信データ
 tx_datas = [
-    CommData(0,  10, 0, lambda: comm_rx_cnt),
-    CommData(1,  10, 1, lambda: g_s32_comm_rx_jdg_red),
+    CommData(0,  10, 0, 0, lambda: comm_rx_cnt),
+    CommData(1,  10, 1, 0, lambda: g_s32_comm_rx_jdg_red),
 
-    CommData(0, None, 100, lambda: send_param[0]),
-    CommData(0, None, 101, lambda: send_param[1]),
-    CommData(0, None, 102, lambda: send_param[2]),
-    CommData(0, None, 103, lambda: send_param[3]),
-    CommData(0, None, 104, lambda: send_param[4]),
-    CommData(0, None, 105, lambda: send_param[5]),
-    CommData(0, None, 106, lambda: send_param[6]),
-    CommData(0, None, 107, lambda: send_param[7]),
-    CommData(0, None, 108, lambda: send_param[8]),
-    CommData(0, None, 109, lambda: send_param[9])
+    CommData(0, None, 100, 0, lambda: send_param[0]),
+    CommData(0, None, 101, 0, lambda: send_param[1]),
+    CommData(0, None, 102, 0, lambda: send_param[2]),
+    CommData(0, None, 103, 0, lambda: send_param[3]),
+    CommData(0, None, 104, 0, lambda: send_param[4]),
+    CommData(0, None, 105, 0, lambda: send_param[5]),
+    CommData(0, None, 106, 0, lambda: send_param[6]),
+    CommData(0, None, 107, 0, lambda: send_param[7]),
+    CommData(0, None, 108, 0, lambda: send_param[8]),
+    CommData(0, None, 109, 0, lambda: send_param[9])
 ]
 
 # 受信データ
 rx_datas = [
-    CommData(0, 100, 500,  comm_tx_cnt),
-    CommData(0, 100, 501,  comm_rx_cnt),
-    CommData(0, 100, 502,  comm_rx_dbg),
+    CommData(0, 100, 500,  0, comm_tx_cnt),
+    CommData(0, 100, 501,  0, comm_rx_cnt),
+    CommData(0, 100, 502,  0, comm_rx_dbg),
 
-    CommData(0, 100, 600,  received_watch[0]),
-    CommData(0, 100, 601,  received_watch[1]),
-    CommData(0, 100, 602,  received_watch[2]),
-    CommData(0, 100, 603,  received_watch[3]),
-    CommData(0, 100, 604,  received_watch[4]),
-    CommData(0, 100, 605,  received_watch[5]),
-    CommData(0, 100, 606,  received_watch[6]),
-    CommData(0, 100, 607,  received_watch[7]),
-    CommData(0, 100, 608,  received_watch[8]),
-    CommData(0, 100, 609,  received_watch[9]),
+    CommData(0, 100, 600,  0, received_watch[0]),
+    CommData(0, 100, 601,  0, received_watch[1]),
+    CommData(0, 100, 602,  0, received_watch[2]),
+    CommData(0, 100, 603,  1, received_watch[3]),
+    CommData(0, 100, 604,  0, received_watch[4]),
+    CommData(0, 100, 605,  0, received_watch[5]),
+    CommData(0, 100, 606,  0, received_watch[6]),
+    CommData(0, 100, 607,  0, received_watch[7]),
+    CommData(0, 100, 608,  0, received_watch[8]),
+    CommData(0, 100, 609,  0, received_watch[9]),
 
-    CommData(0, 100, 700,  received_param[0]),
-    CommData(0, 100, 701,  received_param[1]),
-    CommData(0, 100, 702,  received_param[2]),
-    CommData(0, 100, 703,  received_param[3]),
-    CommData(0, 100, 704,  received_param[4]),
-    CommData(0, 100, 705,  received_param[5]),
-    CommData(0, 100, 706,  received_param[6]),
-    CommData(0, 100, 707,  received_param[7]),
-    CommData(0, 100, 708,  received_param[8]),
-    CommData(0, 100, 709,  received_param[9])
+    CommData(0, 100, 700,  0, received_param[0]),
+    CommData(0, 100, 701,  0, received_param[1]),
+    CommData(0, 100, 702,  0, received_param[2]),
+    CommData(0, 100, 703,  0, received_param[3]),
+    CommData(0, 100, 704,  0, received_param[4]),
+    CommData(0, 100, 705,  0, received_param[5]),
+    CommData(0, 100, 706,  0, received_param[6]),
+    CommData(0, 100, 707,  0, received_param[7]),
+    CommData(0, 100, 708,  0, received_param[8]),
+    CommData(0, 100, 709,  0, received_param[9])
 ]
 
 # シリアルポートの設定
@@ -82,13 +83,28 @@ def set_comm_ui():
         index = 0
         for cmd in range(600,609):
             if cmd == data_info.comm_cmd:
-                received_watch[index] = int(data_info.comm_data)
+                tmp_val = int(data_info.comm_data)
+                print(data_info.comm_cmd)
+                print(data_info.comm_sign)
+                if 0 == data_info.comm_sign:
+                    print("UNSIGN")
+                    received_watch[index] = tmp_val
+                else:
+                    print(tmp_val)
+                    print("SIGN")
+                    if tmp_val >= 32767:
+                        print("OVER")
+                        received_watch[index] = tmp_val - 65536
+                    else:
+                        received_watch[index] = tmp_val
+                    
             index += 1
         index = 0
         for cmd in range(700,709):
             if cmd == data_info.comm_cmd:
                 received_param[index] = int(data_info.comm_data)
             index += 1
+    print(received_watch)
     return received_watch, received_param
 
 def input_comm():
@@ -123,7 +139,7 @@ def received_data():
                 cmd = cmd[1:]
                 received_cmd_buff.append(cmd)
                 received_data_buff.append(value)
-                print(cmd, value)
+                # print(cmd, value)
             else:
                 print("Input string must contain ':' after'@'")
                 return None,None,None
@@ -148,20 +164,21 @@ def cyc_rx():
         cmd, data, buf_num = received_data()
         if buf_num == None:
             return
-        print(cmd)
-        print(data)
+        # print(cmd)
+        # print(data)
         for i in range(buf_num):
             for data_info in rx_datas:
                 # print(cmd[i])
                 # print(data_info.comm_cmd)
                 if cmd == None:
                     continue
-                if int(cmd[i]) == data_info.comm_cmd:
-                    print(int(data[i]))
-                    data_info.comm_data = int(data[i])
-                    print("inf")
-                    print(int(cmd[i]), data_info.comm_data)
-                    break
+                if cmd[i].isdigit():
+                    if int(cmd[i]) == data_info.comm_cmd:
+                        # print(int(data[i]))
+                        data_info.comm_data = int(data[i])
+                        # print("inf")
+                        # print(int(cmd[i]), data_info.comm_data)
+                        break
 
 def close():
     ser.close()  # シリアルポートを閉じる
